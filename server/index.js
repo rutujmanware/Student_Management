0f0d2a9b8b16947583039ba66dfa5c6bd8b105a4
Rituj Manware <manwarerutuj@gmail.com>
2023-10-14 21:21:58 +0530

    backend uri changed

--- server/index.js
+++ server/index.js
@@ -3,14 +3,14 @@ const app = express()
 const mongoDB = require('./db');
 require('dotenv').config();
 
-app.use((req,res,next) => {
-    res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-requested-With, Content-Type, Accept"
-    );
-    next();
-  })
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 mongoDB();
 
